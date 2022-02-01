dd8a6d7a54bb651ce2d6a0342b3b39ce535cdb4d
john <johnarokyaraj@yahoo.in>
2022-02-01 23:33:41 +0530

    App Config Setting

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,16 @@ namespace BidingAPPAPI
         {
             services.AddControllers();
             services.AddControllers().AddNewtonsoftJson();
+            //Add Swagger relates setting  
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Bidding API",
+                    Version = "v1.1",
+                    Description = "API to unerstand request and response schema.",
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +63,11 @@ namespace BidingAPPAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Bidding API");
             });
         }
     }
